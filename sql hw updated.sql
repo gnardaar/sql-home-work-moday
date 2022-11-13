fe20d2cff6e317760471b69cf4a463e6af84d225
andrew kraus <andrewkraus521@gmail.com>
2022-11-13 09:15:06 -0700

    updating...

--- sql hw updated.sql
+++ sql hw updated.sql
@@ -1,41 +1,42 @@
+--question 1
 select * from actor where first_name, last_name  like 'Wahlberg'
 
+--question2
 select count(amount) from payment where amount between 3.99 and 5.99;
 
 
- 
+--question 3
 select * from inventory 
---Inventoty in the store has 8 copies of 72 different films 
 
 
+--question 4
 select last_name,first_name from customer where last_name and first_name  = 'William'
-
-
 SELECT last_name, first_name
 FROM customer
 WHERE first_name LIKE 'William';
-
 SELECT *
 FROM rental;
 
+--question 5
 SELECT COUNT(rental_id), staff_id
 FROM rental
 GROUP BY staff_id;
-
 SELECT *
 FROM rental;
 
+--question 6
 SELECT COUNT(DISTINCT district)
 FROM address;
 
+--question 7
 SELECT *
 FROM film_actor;
-
 SELECT  film_id, COUNT(actor_id) AS total_actors
 FROM film_actor 
 GROUP BY film_id
 ORDER BY total_actors DESC;
 
+--question 8
 SELECT *
 FROM customer;
 
@@ -43,12 +44,12 @@ SELECT first_name, last_name, store_id
 FROM customer
 WHERE last_name LIKE '%es'
 ORDER BY store_id DESC;
-
 SELECT COUNT(last_name) AS es_last_name, store_id
 FROM customer
 WHERE last_name LIKE '%es'
 GROUP BY store_id;
 
+--question 9
 SELECT *
 FROM payment;
 
@@ -59,6 +60,7 @@ GROUP BY amount
 HAVING COUNT(amount) > 250
 ORDER BY amount;
 
+--question 10
 SELECT *
 FROM film;
 
